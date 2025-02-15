362b15aeb49e90e23648369ac05bce89b815830e
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-15 11:56:27 -0600

    Preparación y estructura del proyecto

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,10 +4,10 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import {ComponentsApp} from '../ComponentsApp';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
-    ReactTestRenderer.create(<App />);
+    ReactTestRenderer.create(<ComponentsApp />);
   });
 });

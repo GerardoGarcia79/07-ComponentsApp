c4e320fdbf36db96366a0039298b4d25b081fb6b
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-20 13:49:57 -0600

    Componente - Switch

--- src/presentation/screens/switches/SwitchScreen.tsx
+++ src/presentation/screens/switches/SwitchScreen.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Card} from '../../components/ui/Card';
+import {CustomSwitch} from '../../components/ui/CustomSwitch';
 import {CustomView} from '../../components/ui/CustomView';
 
 export const SwitchScreen = () => {
+  const [state, setState] = useState({
+    isActive: true,
+    isHungry: false,
+    isHappy: true,
+  });
+
   return (
-    // eslint-disable-next-line react/self-closing-comp
-    <CustomView style={{marginTop: 100, paddingHorizontal: 10}}></CustomView>
+    <CustomView style={{marginTop: 100, paddingHorizontal: 10}}>
+      <Card>
+        <CustomSwitch
+          isOn={state.isActive}
+          onChange={value => setState({...state, isActive: value})}
+          text="¿Está activo?"
+        />
+        <CustomSwitch
+          isOn={state.isHungry}
+          onChange={value => setState({...state, isHungry: value})}
+          text="¿Tiene hambre?"
+        />
+        <CustomSwitch
+          isOn={state.isHappy}
+          onChange={value => setState({...state, isHappy: value})}
+          text="¿Es feliz?"
+        />
+      </Card>
+    </CustomView>
   );
 };

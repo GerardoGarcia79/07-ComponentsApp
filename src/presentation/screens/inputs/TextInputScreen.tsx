91e86086afe81e1b2cee5b3b3e734d2073acd265
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-02-22 12:29:13 -0600

    Componente - TextInput

---
+++ src/presentation/screens/inputs/TextInputScreen.tsx
@@ -0,0 +1,76 @@
+import React, {useState} from 'react';
+import {CustomView} from '../../components/ui/CustomView';
+import {Title} from '../../components/ui/Title';
+import {Card} from '../../components/ui/Card';
+import {Text, TextInput, View} from 'react-native';
+import {globalStyles} from '../../../config/theme/theme';
+
+export const TextInputScreen = () => {
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    phone: '',
+  });
+
+  return (
+    <CustomView margin>
+      <Title text="Text Inputs" safe />
+
+      <Card>
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Nombre Completo"
+          autoCapitalize="words"
+          autoCorrect={false}
+          onChangeText={value => setForm({...form, name: value})}
+        />
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Correo electrónico"
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="email-address"
+          onChangeText={value => setForm({...form, email: value})}
+        />
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Teléfono"
+          keyboardType="phone-pad"
+          onChangeText={value => setForm({...form, phone: value})}
+        />
+      </Card>
+
+      <View style={{height: 10}} />
+
+      <Card>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+        <Text>{JSON.stringify(form, null, 2)}</Text>
+      </Card>
+
+      <Card>
+        <TextInput
+          style={globalStyles.input}
+          placeholder="Teléfono"
+          keyboardType="phone-pad"
+          onChangeText={value => setForm({...form, phone: value})}
+        />
+      </Card>
+    </CustomView>
+  );
+};

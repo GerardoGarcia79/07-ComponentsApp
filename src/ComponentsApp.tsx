7076420cd86ce95d109835c26cb96209253d06d5
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-04-02 10:18:26 -0600

    React Navigation Theme

--- src/ComponentsApp.tsx
+++ src/ComponentsApp.tsx
@@ -1,22 +1,36 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
+import {
+  DarkTheme,
+  DefaultTheme,
+  NavigationContainer,
+} from '@react-navigation/native';
+import React, {useContext} from 'react';
 import '../gesture-handler';
 import {StackNavigator} from './presentation/navigator/StackNavigator';
 import {PropsWithChildren} from 'react';
-import {ThemeProvider} from './presentation/context/ThemeContext';
+import {ThemeContext, ThemeProvider} from './presentation/context/ThemeContext';
 
-const AppState = ({children}: PropsWithChildren) => {
+const AppNavigation = ({children}: PropsWithChildren) => {
+  const {isDark} = useContext(ThemeContext);
+
   return (
-    <NavigationContainer>
-      <ThemeProvider>{children}</ThemeProvider>
+    <NavigationContainer theme={isDark ? DarkTheme : DefaultTheme}>
+      {children}
     </NavigationContainer>
+  );
+};
+
+const AppTheme = ({children}: PropsWithChildren) => {
+  return (
+    <ThemeProvider>
+      <AppNavigation>{children}</AppNavigation>
+    </ThemeProvider>
   );
 };
 
 export const ComponentsApp = () => {
   return (
-    <AppState>
+    <AppTheme>
       <StackNavigator />
-    </AppState>
+    </AppTheme>
   );
 };
